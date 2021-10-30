03d121bbe240e687c8e2f378ad425ab7e25e56bf
arunstop <arunstop@gmail.com>
2021-10-31 00:47:52 +0700

    FIXED dialog import ui

--- src/renderer/components/Dialogs/DialogImport.vue
+++ src/renderer/components/Dialogs/DialogImport.vue
@@ -3,9 +3,10 @@
     <v-card outlined>
       <v-card-title class="primary--text">Import accounts</v-card-title>
       <v-card-text>
+        <!-- HIDDEN FILE INPUT -->
         <v-file-input
           ref="importFileInput"
-          v-model="files"
+          v-model="filesPseudo"
           class="d-none"
           color="primary accent-4"
           counter
@@ -16,6 +17,7 @@
           multiple
           :show-size="1000"
           accept=".json"
+          @change.native="onchange"
         >
           <template #selection="{ index, text }">
             <v-chip color="primary accent-4" label small>
@@ -24,51 +26,60 @@
           </template>
         </v-file-input>
         <v-expand-transition>
+          <!-- IMPORT PANEL -->
           <v-card
             v-if="!files.length"
+            v-bind="importPanel.loading"
             outlined
             height="300"
-            class="d-flex align-center justify-center"
-            :color="importPanelColor"
-             @click="uploadFiles()"
+            class=""
+            :color="importPanel.color"
+            @click="uploadFiles()"
             @dragover="dragover"
             @dragleave="dragleave"
             @drop="drop"
           >
-            <div class="text-center" style="pointer-events:none;">
-              <v-btn color="primary" large>
-                UPLOAD
-                <v-icon>mdi-upload</v-icon>
-              </v-btn>
+            <div class="d-flex align-center justify-center fill-height">
+              <div class="text-center" :class="importPanel.button">
+                <v-btn color="primary" large>
+                  UPLOAD
+                  <v-icon>mdi-upload</v-icon>
+                </v-btn>
 
-              <h4 class="mt-4">
-                Click the button above or drag files here.
-              </h4>
+                <h4 class="mt-4">Click the button above or drag files here.</h4>
+              </div>
             </div>
           </v-card>
           <div v-else>
-            <v-card class="my-4" outlined>
-              <div class="d-flex justify-space-between flex-wrap px-2 py-4">
-                <v-scale-transition group>
-                  <v-chip
-                  v-for="(file, index) in fileList"
-                  :key="file.name"
-                  class="ma-2 py-2 white--text"
-                  color="indigo"
-                  label
-                  close
-                  style="max-width: 240px"
-                  @click:close="removeFile(file.name)"
+            <v-card class="mb-4" outlined>
+              <v-card
+                class="my-4 overflow-y-scroll elevation-0"
+                color="transparent"
+                max-height="300"
+              >
+                <v-fade-transition
+                  class="d-flex ps-4 justify-space-between flex-wrap"
+                  group
                 >
-                  <span class="text-truncate">{{
-                    index + 1 + ". " + file.name
-                  }}</span>
-                  <span class="ms-1 text-decoration-underline">
-                    {{ file.accList.length + " accounts" }}
-                  </span>
-                </v-chip>
-                </v-scale-transition>
-              </div>
+                  <v-chip
+                    v-for="(file, index) in fileList"
+                    :key="file.name"
+                    class="ma-2 py-2 white--text"
+                    color="indigo"
+                    label
+                    close
+                    @click:close="removeFile(file.name)"
+                  >
+                    <span class="text-truncate">{{
+                      index + 1 + ". " + file.name
+                    }}</span>
+                    <span class="ms-1 text-decoration-underline">
+                      {{ file.accList.length + " accounts" }}
+                    </span>
+                  </v-chip>
+                </v-fade-transition>
+              </v-card>
+
               <v-divider />
               <div class="pa-4 text-right">
                 <v-btn
@@ -77,7 +88,7 @@
                   small
                   outlined
                   rounded
-                  @click="files = []"
+                  @click="clearFiles()"
                 >
                   Clear
                   <v-icon right>mdi-close</v-icon>
@@ -90,8 +101,8 @@
                   rounded
                   @click="uploadFiles()"
                 >
-                  Edit
-                  <v-icon right>mdi-pencil</v-icon>
+                  Add
+                  <v-icon right>mdi-plus</v-icon>
                 </v-btn>
               </div>
             </v-card>
@@ -155,13 +166,13 @@
 </template>
 
 <script>
-import { remote } from "electron";
 export default {
   data() {
     return {
+      filesPseudo: [],
       files: [],
       option: "",
-      importPanelColor:'',
+      importPanel: { color: "", button: "", loading: {} },
     };
   },
   computed: {
@@ -177,7 +188,7 @@ export default {
       const fileDetailList = [];
       this.files.forEach((e) => {
         const accList = this.getAccountList(e);
-        console.log(this.$globals.lodash.assign(e, { accList }));
+        // console.log(this.$globals.lodash.assign(e, { accList }));
         fileDetailList.push(this.$globals.lodash.assign(e, { accList }));
       });
       return fileDetailList || [];
@@ -219,27 +230,63 @@ export default {
       const fs = require("fs");
       return JSON.parse(fs.readFileSync(file.path));
     },
+    clearFiles() {
+      this.files = [];
+    },
     removeFile(name) {
       this.files = this.files.filter((e) => e.name !== name);
     },
     uploadFiles() {
       this.$refs.importFileInput.$refs.input.click();
     },
-    dragover(event){
-      event.preventDefault()
-      this.importPanelColor='indigo'
+    onchange(event) {
+      if (!event.target.files.length) return;
+      const fetchedFiles = Array.from(event.target.files);
+      // console.log(this.files);
+      if (!this.files.length) {
+        this.files = fetchedFiles;
+        return;
+      }
+      fetchedFiles.forEach((e) => {
+        if (!this.files.find((f) => f.path === e.path)) this.files.push(e);
+      });
+      // console.log(this.files);
     },
-    dragleave(event){
-      event.preventDefault()
-      this.importPanelColor=''
+    toggleImportPanelLoading(v) {
+      if (v) {
+        this.importPanel.loading = { loading: true, disabled: true };
+      }
+      this.importPanel.loading = {};
     },
-    drop(event){
-      event.preventDefault()
-      const fileList = Array.from(event.dataTransfer.files)
-      const validFiles = fileList.filter(e=>e.name.toLowerCase().trim().split('.').reverse()[0]==='json')
-      this.importPanelColor=''
-      this.files= validFiles
-    }
+    toggleImportPanel(v) {
+      if (v) {
+        this.importPanel.color = "indigo";
+        this.importPanel.button = "pointer-events-none";
+        return;
+      }
+      this.importPanel.color = "";
+      this.importPanel.button = "";
+    },
+    dragover(event) {
+      event.preventDefault();
+      this.toggleImportPanel(true);
+    },
+    dragleave(event) {
+      event.preventDefault();
+      this.toggleImportPanel(false);
+    },
+    async drop(event) {
+      event.preventDefault();
+      this.toggleImportPanelLoading(true)
+      const fileList = Array.from(event.dataTransfer.files);
+      const validFiles = fileList.filter(
+        (e) => e.name.toLowerCase().trim().split(".").reverse()[0] === "json"
+      );
+      this.importPanelColor = "";
+      this.files = validFiles;
+      this.toggleImportPanelLoading(false)
+      this.toggleImportPanel(false);
+    },
   },
 };
 </script>
