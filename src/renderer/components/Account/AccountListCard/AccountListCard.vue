7d1b800f59d057dc9c98471e12481188d07ff87a
arunstop <arunstop@gmail.com>
2021-11-17 21:55:49 +0700

    ADDED different [not found template] on account list card view when there are no account found

--- src/renderer/components/Account/AccountListCard/AccountListCard.vue
+++ src/renderer/components/Account/AccountListCard/AccountListCard.vue
@@ -19,9 +19,9 @@
       style="min-height: 300px; position: absolute"
     >
       <v-card class="ma-auto" elevation="0">
-        <v-card-text class="d-flex flex-column text-center">
+        <v-card-text v-if="$store.state.account.accountList.length" class="d-flex flex-column text-center">
           <v-icon size="90">mdi-robot-confused</v-icon>
-          <h3 class="font-weight-bold">No data found.</h3>
+          <h3 class="font-weight-bold">No matches were found.</h3>
           <v-btn
             class="mt-4 font-weight-bold"
             outlined
@@ -32,12 +32,26 @@
             <v-icon right>mdi-reload</v-icon>
           </v-btn>
         </v-card-text>
+        <v-card-text v-else class="d-flex flex-column text-center">
+          <v-icon size="90">mdi-robot-confused</v-icon>
+          <h3 class="font-weight-bold">You account list is currently empty.</h3>
+          <v-btn
+            class="mt-4 font-weight-bold"
+            outlined
+            color="primary"
+            @click="openAccountAddDialog()"
+          >
+            Add Account
+            <v-icon right>mdi-plus-circle-outline</v-icon>
+          </v-btn>
+        </v-card-text>
       </v-card>
     </div>
   </div>
 </template>
 
 <script>
+import {mapActions} from 'vuex'
 export default {
   props: {
     data: { type: Array, default: () => [] }
@@ -48,9 +62,13 @@ export default {
     }
   },
   methods: {
+    ...mapActions('ui', ['toggleDialog']),
     resetSearch () {
       this.$store.dispatch('account/setAccountSearch', '')
-    }
+    },
+   openAccountAddDialog () {
+      this.toggleDialog({ type: 'ACCOUNT_ADD_DIALOG', val: true })
+    },
   }
 }
 </script>
